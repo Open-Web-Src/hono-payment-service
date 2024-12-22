23215d4571989b3d4d0acb774593f2d2bf1cc731
@kimhiepninh02121997 <kimhiepninh02121997@gmail.com>
2024-12-22 15:14:13 +0700

    fix: test

---
+++ migrations/0010_public_dust.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `tests` (
+	`id` text PRIMARY KEY NOT NULL,
+	`created_at` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now')) NOT NULL,
+	`updated_at` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now')) NOT NULL,
+	`deleted_at` text
+);
